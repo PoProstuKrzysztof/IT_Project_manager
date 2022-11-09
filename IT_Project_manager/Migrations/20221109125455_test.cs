22fa5f8fdea92fd716c9c267e6768c415ddd0130
Krzysztof Palonek <krzysiek.palonek@gmail.com>
2022-11-09 14:24:23 +0100

    Adding local database

---
+++ IT_Project_manager/Migrations/20221109125455_test.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ITProjectmanager.Migrations
+{
+    /// <inheritdoc />
+    public partial class test : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Members",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Surname = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Members", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Members",
+                columns: new[] { "Id", "Email", "Name", "Surname" },
+                values: new object[,]
+                {
+                    { 5, "krzysiek.palonek@gmail.com", "Krzysztof", "Palonek" },
+                    { 6, "marz.koł@gmail.com", "Marzena", "Kołodziej" },
+                    { 7, "jan.kow@gmail.com", "Jan", "Kowalski" },
+                    { 8, "Nat.uro@gmail.com", "Natalia", "Urodek" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Members");
+        }
+    }
+}
---
+++ IT_Project_manager/Models/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using Microsoft.EntityFrameworkCore.Design;
+
+
+
+
+namespace IT_Project_manager.Models;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Member> Members { get; set; }
+
+    public string DbPath { get; set; }
+
+
+    public AppDbContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath( folder );
+        DbPath = System.IO.Path.Join( path, "members.db" );
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite( $"Data Source = {DbPath}" );
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Member>().HasData(
+             new Member() { Name = "Krzysztof", Surname = "Palonek", Email = "krzysiek.palonek@gmail.com" },
+            new Member() { Name = "Marzena", Surname = "Kołodziej", Email = "marz.koł@gmail.com" },
+            new Member() { Name = "Jan", Surname = "Kowalski", Email = "jan.kow@gmail.com" },
+            new Member() { Name = "Natalia", Surname = "Urodek", Email = "Nat.uro@gmail.com" }
+            );
+    }
+}
