69c50e20b0eea1ee44d05f17a438bb4379435c2f
JustChris <krzysiek.palonek@gmail.com>
2023-01-19 23:33:25 +0100

    19.01

--- IT_Project_manager/Areas/Identity/Data/ApplicationUser.cs
+++ IT_Project_manager/Areas/Identity/Data/ApplicationUser.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Xunit.Sdk;
 
 namespace IT_Project_manager.Areas.Identity.Data;
 
@@ -10,7 +12,10 @@ public class ApplicationUser : IdentityUser
     [Column( TypeName = "nvarchar(100)" )]
     public string? Name { get; set; }
 
-    [PersonalData]
-    [Column( TypeName = "nvarchar(100)" )]
-    public string? Surname { get; set; }
+    [Required]
+    [RegularExpression( "^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid" )]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    public string Password { get; set; } = string.Empty;
 }
--- IT_Project_manager/Areas/Identity/Repository/IRoleRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace IT_Project_manager.Areas.Identity.Repository;
-
-
-public interface IRoleRepository
-{
-    ICollection<IdentityRole> GetRoles();
-}
